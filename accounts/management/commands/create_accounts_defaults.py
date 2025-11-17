ad1c7f09fdbc98c8aee1a44e66aaa3f2b8cbb394
Alperen korana <alperenkorana.izmir@gmail.com>
2025-11-17 12:09:51 +0300

    accounts: cleanup create_accounts_defaults imports, docstring, fix unused vars and output formatting

--- accounts/management/commands/create_accounts_defaults.py
+++ accounts/management/commands/create_accounts_defaults.py
@@ -1,9 +1,11 @@
-"""Management command to create default groups and departments."""
+"""Management command to create default groups, departments and titles.
+
+This command is safe to run multiple times; it will create missing
+groups/departments/titles and assign permissions to the 'YÖNETİCİ' group.
+"""
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import Group, Permission
-from django.contrib.contenttypes.models import ContentType
 from accounts.models import Department, Title
-from django.contrib.auth import get_user_model
 
 DEFAULT_GROUPS = [
     "KENTSEL DÖNÜŞÜM UZMANI",
@@ -36,27 +38,28 @@ DEFAULT_TITLES = [
 
 
 class Command(BaseCommand):
+    """Create default groups, departments and titles."""
+
     help = "Create default groups, departments and titles"
 
     def handle(self, *args, **options):
         created = []
         for g in DEFAULT_GROUPS:
-            grp, ok = Group.objects.get_or_create(name=g)
-            if ok:
+            _, created_flag = Group.objects.get_or_create(name=g)
+            if created_flag:
                 created.append(f"group:{g}")
         for d in DEFAULT_DEPARTMENTS:
-            dep, ok = Department.objects.get_or_create(name=d)
-            if ok:
+            _, created_flag = Department.objects.get_or_create(name=d)
+            if created_flag:
                 created.append(f"department:{d}")
         for t in DEFAULT_TITLES:
-            tit, ok = Title.objects.get_or_create(name=t)
-            if ok:
+            _, created_flag = Title.objects.get_or_create(name=t)
+            if created_flag:
                 created.append(f"title:{t}")
 
         # Give 'YÖNETİCİ' group all permissions (like superuser)
         try:
             admin_grp = Group.objects.get(name="YÖNETİCİ")
-            # assign all permissions
             perms = Permission.objects.all()
             admin_grp.permissions.set(perms)
             admin_grp.save()
@@ -64,4 +67,4 @@ class Command(BaseCommand):
         except Group.DoesNotExist:
             pass
 
-        self.stdout.write("Created: %s" % (", ".join(created)))
+        self.stdout.write(f"Created: {', '.join(created)}")
